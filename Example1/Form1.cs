072080ab18002cc247ca9b72993cb2d316db42d3
Hau21022003 <lephuchau21022003@gmail.com>
2024-01-29 01:19:39 +0700

    second

--- Example1/Form1.cs
+++ Example1/Form1.cs
@@ -1,7 +1,8 @@
-namespace Example1
+﻿namespace Example1
 {
     public partial class Form1 : Form
     {
+        // cập nhật lần 1
         public Form1()
         {
             InitializeComponent();
