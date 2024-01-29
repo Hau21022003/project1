15a1fd221fcadfbd52d37a870ab81d77eb37ee9a
Hau21022003 <lephuchau21022003@gmail.com>
2024-01-29 18:04:19 +0700

    cap nhat nhanh con lan 4

--- Example1/Form1.cs
+++ Example1/Form1.cs
@@ -6,6 +6,7 @@
         // Cập nhật nhánh con
         // cập nhật nhánh con lần 2
         // cap nhat nhanh con lần 3
+        // cap nhat nhanh con lần 4
         public Form1()
         {
             InitializeComponent();
